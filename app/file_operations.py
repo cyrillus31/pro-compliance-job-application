78e0bcda6147c04288804241ca51987ef4ad164a
Kirill F <kirill.olegovich31@gmail.com>
2023-07-23 22:00:20 +0300

    filtering with csv implemented but no path operation

--- app/file_operations.py
+++ app/file_operations.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import re
 
 
 class Csv:
@@ -24,8 +25,40 @@ class Csv:
                 result[file] = {"columns": reader.fieldnames}
         return result
 
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, filename):
+        Csv.update_paths()
+        if filename in Csv.files:
+            self.path = Csv._folder_path + "/" + filename
+        else:
+            raise Exception("This file doesn't exist on a server")
+
+    def filter_data(
+        self,
+        column_to_filter: str,
+        list_of_columns_to_display: list[str],
+        like: str = "",
+    ):
+        result = {}
+        with open(self.path, "r", encoding="utf-8") as csv_file:
+            reader = csv.DictReader(csv_file)
+            if column_to_filter not in reader.fieldnames:
+                raise Exception("This column doesn't exist in this document")
+            for row in reader:
+                if like in row[column_to_filter]:
+                    for column in list_of_columns_to_display:
+                        if column not in result:
+                            result[column] = [row[column]]
+                        else:
+                            result[column].append(row[column])
+
+        return result
 
 
-# print(Csv.get_all_files_all_columns())
+mycsv = Csv("Electric_Vehicle_Population_Data.csv")
+print(
+    mycsv.filter_data(
+        column_to_filter="City",
+        list_of_columns_to_display=["City", "State", "Make"],
+        like="Seattle",
+    )
+)
