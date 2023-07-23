42dc039b171756feed7dc8c64c68cd28c3d0a71d
Kirill F <kirill.olegovich31@gmail.com>
2023-07-23 17:40:11 +0300

    added file upload exseption handling & added get/{extensio} path operation

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import shutil
 
 from fastapi import FastAPI, HTTPException, UploadFile, status
 
+
 from fastapi.responses import HTMLResponse
 
 from . import utils
@@ -15,18 +16,35 @@ app = FastAPI()
 def root():
     with open("app/templates/index.html", "r") as tmp:
         content = tmp.read()
+
         return HTMLResponse(content=content, status_code=200)
 
 
-@app.post("/upload_csv", status_code=status.HTTP_201_CREATED)
+@app.post("/upload", status_code=status.HTTP_201_CREATED)
 def upload_file(file: UploadFile):
-    utils.create_uploads_folders()
-    print(file, type(file))
     if not file.filename:
-        raise HTTPException(status_code=400)
-    else:
-        path = utils.get_the_path(file.filename) + f"/{file.filename}"
-        with open(path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
+        raise HTTPException(
+            status_code=400, detail="Provide a file with valid extension"
+        )
 
-        return {"filename": file.filename}
+    extension = utils.get_extension(file.filename)
+
+    if utils.is_valid_extension(extension):
+        utils.create_uploads_folder(extension)
+        utils.save_file(file)
+
+        return {
+            "filename": file.filename,
+            "detail": "File saved on server successfully",
+        }
+
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Files of {extension} format are not yet supported",
+        )
+
+
+@app.get("/{extension}", status_code=status.HTTP_200_OK)
+def list_files(extension: str):
+    return utils.get_list_of_all_files_by_extension(extension)
--- app/utils.py
+++ app/utils.py
@@ -1,15 +1,60 @@
 import os
 
 
-upload_folders = {"csv": "./app/data"}
+import shutil
 
 
-def create_uploads_folders():
-    for folder in upload_folders.values():
-        if not os.path.exists(folder):
-            os.mkdir(folder)
+import csv
 
 
-def get_the_path(file: str) -> str:
-    extension = os.path.splitext(file)[1].strip(".")
-    return upload_folders[extension]
+# create data directory
+
+
+if not os.path.exists("./app/data"):
+    os.mkdir("./app/data")
+
+
+# provide paths to folders for specific file extensions
+
+
+_upload_folders = {
+    "csv": "./app/data/csv",
+}
+
+
+def is_valid_extension(extension: str) -> bool:
+    return extension in _upload_folders
+
+
+def create_uploads_folder(extension):
+    folder = _upload_folders[extension]
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+
+def get_extension(filename: str) -> str:
+    extension = os.path.splitext(filename)[1].strip(".")
+    return extension
+
+
+def get_path(extension) -> str:
+    return _upload_folders[extension]
+
+
+def save_file(file) -> bool:
+    extension = get_extension(file.filename)
+
+    filename = file.filename.replace(" ", "_")
+
+    path = get_path(extension) + f"/{filename}"
+
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+
+def get_list_of_all_files_by_extension(extension: str) -> list[str]:
+    path = _upload_folders[extension]
+
+    root, folders, files = next(os.walk(path))
+    return files
